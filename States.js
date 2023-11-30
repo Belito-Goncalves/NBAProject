4ef5e14c428e445a477f26fed2d967fe03fdfbfb
Belito-Goncalves <gabfacgon@gmail.com>
2023-11-30 14:57:15 +0000

    Loading Modal

---
+++ States.js
@@ -0,0 +1,31 @@
+showLoading();
+    var pg = getUrlParameter('page');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+;
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
+
+function showLoading() {
+    $("#myModal").modal('show', {
+        backdrop: 'static',
+        keyboard: false
+    });
+}
+function hideLoading() {
+    $('#myModal').on('shown.bs.modal', function (e) {
+        $("#myModal").modal('hide');
+    })
+}
--- Teams.js
+++ Teams.js
@@ -1,15 +1,19 @@
-function showLoading() {
-    $("#myModal").modal('show', {
-        backdrop: 'static',
-        keyboard: false
-    });
-}
-function hideLoading() {
-    $('#myModal').on('shown.bs.modal', function (e) {
-        $("#myModal").modal('hide');
-    })
-}
+showLoading();
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
 
 $(document).ajaxComplete(function (event, xhr, options) {
-    $("#myModal").modal('hide');
-})
+    hideLoading(); // Call hideLoading when the Ajax request is complete
+});
+
+var pg = getUrlParameter('page');
+console.log(pg);
+if (pg == undefined)
+    self.activate(1);
+else {
+    self.activate(pg);
+}
+console.log("VM initialized!");
